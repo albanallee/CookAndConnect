9325f39a04da4fcd66cdb7efcbce570dc544906c
Lucas Labigne <lucas.labigne@gmail.com>
2020-03-27 15:30:07 +0100

    intégration modal

--- src/components/ModalPassword.vue
+++ src/components/ModalPassword.vue
@@ -1,20 +1,66 @@
 <template>
-    <div>
-        
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-danger" id="exampleModalLabel">Avant de télécharger Votre Application</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h4>Merci d'entrer le mot de passe que vous avez reçu par mail</h4>
+          <div class="form-group d-flex">
+            <label for="exampleInputPassword1">Mot de passe</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+          </div>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ModalPassword',
-    data () {
-        return {
-
-        }
-    }
-}
+  name: "ModalPassword",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style scoped lang="less">
-
+.modal {
+  top: 25%;
+}
+.form-group {
+  justify-content: space-around;
+  margin: 2rem 0rem;
+}
+.modal-body {
+  > h4 {
+    font-weight: normal;
+  }
+}
+button.close {
+  box-shadow: none;
+  -webkit-box-shadow: none;
+}
 </style>
