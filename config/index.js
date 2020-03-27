9bf87029fa2d668b9414c059f1ea94a3c14dd9e1
HAVAS Augustin <ahavas412@gmail.com>
2020-03-27 14:51:09 +0100

    fix img path

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '/CookAndConnect/static',
-    assetsPublicPath: '/CookAndConnect',
+    assetsSubDirectory: 'CookAndConnect/static',
+    assetsPublicPath: '/CookAndConnect/',
 
     /**
      * Source Maps
