fae665be0cf5b7bb9fa6410e39211777708816be
HAVAS Augustin <ahavas412@gmail.com>
2020-03-27 15:01:58 +0100

    css priority

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "@popperjs/core";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap-vue/dist/bootstrap-vue";
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
 new Vue({
   el: '#app',
