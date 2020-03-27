962df42af9e2087112ab000ea171d17d57b74a68
HAVAS Augustin <ahavas412@gmail.com>
2020-03-27 09:40:31 +0100

    fix url assets

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'CookAndConnect/static',
+    assetsSubDirectory: 'CookAndConnect/CookAndConnect/static',
     assetsPublicPath: '/',
 
     /**
