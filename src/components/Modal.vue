67adbe6bc081ac829e71c5e809552ebe16832a97
HAVAS Augustin <ahavas412@gmail.com>
2020-04-20 15:00:16 +0200

    Update Log Modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,40 +1,44 @@
 <template>
-  <!-- Modal -->
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    role="dialog"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title text-danger" id="exampleModalLabel">Avant de télécharger Votre Application</h5>
-          <button
-            type="button"
-            class="close"
-            data-dismiss="modal"
-            aria-label="Close"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <h4>Merci d'entrer le mot de passe que vous avez reçu par mail</h4>
-          <div class="form-group d-flex">
-            <label for="exampleInputPassword1">Mot de passe</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+  <div>
+    <!-- Modal Log-->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-danger" id="exampleModalLabel">Avant de télécharger Votre Application</h5>
+            <button
+              type="button"
+              class="close exit-js"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-          <div class="spinner" style="display: flex; justify-content:center;"><img style="display: none; width: 30px; height: auto; text-align: center;" :src="require('@/assets/spinner.gif')" alt="spinner"></div>
-          <button type="button" class="btn btn-secondary close-js" data-dismiss="modal">
-            Close
-          </button>
-          <button type="button" class="btn btn-primary js-send">Save changes</button>
+          <div class="modal-body">
+            <h4>Merci d'entrer le mot de passe que vous avez reçu par mail</h4>
+            <div class="form-group d-flex">
+              <label for="exampleInputPassword1">Mot de passe</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+            </div>
+            <div class="spinner" style="display: flex; justify-content:center;"><span></span><img style="display: none; width: 30px; height: auto; text-align: center;" :src="require('@/assets/spinner.gif')" alt="spinner"></div>
+            <button type="button" class="btn btn-secondary close-js" data-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary js-send">Save changes</button>
+          </div>
         </div>
       </div>
     </div>
+    <!-- Modal List -->
+    
   </div>
 </template>
 
@@ -49,8 +53,8 @@ $(function() {
       hash: hash,
       password: password
     };
-    console.log(datas);
   
+    // Requête qui vérifie le mot de passe et qui fais les petites animations si le mdp est bon ou pas
     $.ajax({
       url: apiUrl,
       headers: {
@@ -61,17 +65,24 @@ $(function() {
       data: datas,
     }).done(function (response) {
       $('div.spinner > img').css('display', 'none');
-      console.log(response);
       if (response.ok) {
-        console.log('ok');
-        $('div.spinner').text('Mot de passe correct !');
+        $('div.spinner > span').text('Mot de passe correct !');
         setTimeout(function() {
+          // Enregistrer l'authentification en session ou dans un cookie
           $('button.close-js').click();
         }, 1000)
       } else {
-        $('div.spinner').text('Mot de passe incorrect, réessayez !');
+        $('div.spinner > span').text('Mot de passe incorrect, réessayez !');
       }
-    });
+  });
+  });
+  $('button.close-js').click(function() {
+  $('input.form-control').val('');
+    $('div.spinner > span').text('');
+  });
+  $('button.exit-js').click(function() {
+    $('input.form-control').val('');
+    $('div.spinner > span').text('');
   });
 });
 export default {
